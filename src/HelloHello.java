24e6000fbf24a5dcc4d55fd9579f2c2df42d73c9
Rabiya Dar <rabiya_khalid@hotmail.com>
2023-11-09 10:33:31 +0100

    Initial commit adding two files

---
+++ src/HelloHello.java
@@ -0,0 +1,17 @@
+public class HelloHello {
+    public class Helloworld {
+
+        public static void main(String[] args) {
+            String text = "hej";
+            System.out.println(text);
+
+            System.out.println(text + "då");
+
+            text = text + " då";
+            System.out.println(text);
+
+
+
+            System.out.println("Hello World!!");
+        }
+}
